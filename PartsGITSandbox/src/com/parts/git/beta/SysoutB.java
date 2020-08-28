04680e7cd7271572b4ff094b8b430f206bf55e5d
Will Garnett <will.garnett@ontario.ca>
2020-08-28 10:38:52 -0400

    Initial entry of a bunch of artifacts.

---
+++ PartsGITSandbox/src/com/parts/git/beta/SysoutB.java
@@ -0,0 +1,9 @@
+package com.parts.git.beta;
+
+public class SysoutB {
+
+	public static void main(String[] args) {
+		System.out.println("B");
+	}
+
+}
---
+++ PartsGITSandbox/src/com/parts/git/beta/SysoutC.java
@@ -0,0 +1,9 @@
+package com.parts.git.beta;
+
+public class SysoutC {
+
+	public static void main(String[] args) {
+		System.out.println("C");
+	}
+
+}
